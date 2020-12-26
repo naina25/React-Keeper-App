d841d8e0408beaffd63465c49dd153dcb6b3bf97
naina <nanu02d8@gmail.com>
2020-12-26 16:27:15 +0530

    Add Footer Component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./Footer";
 import Header from "./Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
