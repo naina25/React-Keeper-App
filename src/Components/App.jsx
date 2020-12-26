00c7d4a6fb7a4e7dada0df455feb70a1ea5f8eb3
naina <nanu02d8@gmail.com>
2020-12-26 16:29:06 +0530

    Add Notes component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import Note from "./Note";
 
 function App() {
   return (
     <div>
       <Header />
+      <Note />
       <Footer />
     </div>
   );
---
+++ src/Components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Note = () => {
+  return (
+    <div className="note">
+      <h1>This is a note title</h1>
+      <p>This is a note content</p>
+    </div>
+  );
+};
+
+export default Note;
